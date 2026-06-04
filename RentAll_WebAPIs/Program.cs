76013e8bba7af132c580c2d0d79495384e8f5bd4
mfaisaltech244 <137287107+mfaisaltech244@users.noreply.github.com>
2026-06-04 17:53:38 +0500

    Configured CORS policy for Angular frontend integration
    Added and configured a CORS policy in Program.cs to allow requests from the Angular frontend (http://localhost:4200). Enabled cross-origin communication by allowing all headers and HTTP methods to support seamless interaction between the ASP.NET Web API and Angular application.

--- RentAll_WebAPIs/Program.cs
+++ RentAll_WebAPIs/Program.cs
@@ -6,8 +6,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("angular", builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
+
+app.UseCors("angular");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
