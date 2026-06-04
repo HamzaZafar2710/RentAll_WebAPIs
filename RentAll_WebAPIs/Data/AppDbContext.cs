95f2e473e050506c736b1613573a71ebf3862f7d
AdeMaq <160094094+AdeMaq@users.noreply.github.com>
2026-06-04 17:24:32 +0500

    Initial setup: packages and basic swagger setup

--- RentAll_WebAPIs/Data/AppDbContext.cs
+++ RentAll_WebAPIs/Data/AppDbContext.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace RentAll_WebAPIs.Data
 {
-    public class AppDbContext : Controller
+    public class AppDbContext : DbContext
     {
-        public IActionResult Index()
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            return View();
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                entity.SetTableName(entity.GetTableName()!.ToLower());
+                foreach (var prop in entity.GetProperties())
+                    prop.SetColumnName(prop.GetColumnName().ToLower());
+            }
+
+
         }
     }
 }
